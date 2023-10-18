677634281440f0cda56ec8800033ef63dbb1dff8
diegomesaluna <diego.mesa@tresastronautas.com>
2023-10-18 07:06:16 -0500

    fix: agrego el class-validator.

--- src/presentation/controllers/producer.controller.ts
+++ src/presentation/controllers/producer.controller.ts
@@ -1,7 +1,7 @@
 import { Body, Controller, Get, Post } from "@nestjs/common";
 import { ApiTags } from "@nestjs/swagger";
 import { IProducerUseCases } from "src/application/ports/UseCases/ProducerUseCases/IProducerUseCases";
-import { producerDto } from "../view-models/producer.dto";
+import { ProducerDto } from "../view-models/producer.dto";
 
 @Controller("producer")
 @ApiTags('Producer')
@@ -10,7 +10,7 @@ export class ProducerController {
     constructor(private readonly producerUseCase: IProducerUseCases) { }
 
     @Post('create')
-    async createProducer(@Body() data: producerDto) {
+    async createProducer(@Body() data: ProducerDto) {
         return await this.producerUseCase.createProducer(data);
     }
 
--- src/presentation/view-models/producer.dto.ts
+++ src/presentation/view-models/producer.dto.ts
@@ -1,6 +1,8 @@
 import { ApiProperty } from "@nestjs/swagger";
+import { IsString } from "class-validator";
 
-export class producerDto {
+export class ProducerDto {
+    @IsString()
     @ApiProperty()
     user: string;
 }
