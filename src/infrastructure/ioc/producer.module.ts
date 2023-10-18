69af9a2507cdc640e254f0be8439a261a20f5369
diegomesaluna <diego.mesa@tresastronautas.com>
2023-10-18 00:33:11 -0500

    fix: cambio el host.

--- src/infrastructure/ioc/producer.module.ts
+++ src/infrastructure/ioc/producer.module.ts
@@ -11,7 +11,7 @@ import { ProducerController } from "src/presentation/controllers/producer.contro
                 name: 'consumer-Integration',
                 transport: Transport.RMQ,
                 options: {
-                    urls: ['amqp://localhost:5672'],
+                    urls: ['amqp://rabbitmq:5672'],
                     queue: 'cats_queue',
                     queueOptions: {
                         durable: false
